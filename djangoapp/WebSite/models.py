971aca5715c0f0e36fb6b34793b1e95bc1cb5015
JoelSepulvedaMartins <SepulvedaJoelMartins@outlook.com>
2023-11-25 02:43:47 -0300

    Desenvolvendo grande parte backend e banco de dados, usando front para testes, a principio ambos sao para contruçao base do desenvolvimento

--- djangoapp/WebSite/models.py
+++ djangoapp/WebSite/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 ##Django adiciona o id por padrao
 class Services(models.Model):
-    name = models.CharField(max_length=25)
-    email = models.EmailField(max_length=150)
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=120)
     message = models.TextField(max_length=1000)  
     telephone = models.CharField(max_length=20)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- djangoapp/WebSite/urls.py
+++ djangoapp/WebSite/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from WebSite.views import index, nossos_servicos,servicosAdmin
-
+from WebSite.views import index, nossos_servicos,servicosAdmin,see_service,serviceDelete
+from WebSite import views
 
 app_name = 'WebSite'
 urlpatterns = [
-    # WebSite:index    url reverse, como buscar
+    # WebSite:index   WebSite:servicosAdmin .....   url reverse, como buscar
     path('', index, name='index'),
-    path('nossos_servicos', nossos_servicos),
-    path('servicosAdmin', servicosAdmin),
+    path('nossos_servicos/', nossos_servicos,name='nossos_servicos'), # type: ignore
+    path('servicosAdmin/', servicosAdmin, name='servicosAdmin'), # type: ignore
+    path('see_service/<int:id>', see_service, name='see_service'), # type: ignore , recebendo parametro na url
+    path('serviceDelete/<int:id>', serviceDelete, name='serviceDelete'),
 ]
